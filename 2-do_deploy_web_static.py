2a4ee3fad17ab05d6c4924b7b7eaed368f7c0aab
Raheem Salifu Kasim <r.kasim@aol.com>
2022-12-23 01:01:34 +0000

    Task Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,68 +1,64 @@
 #!/usr/bin/python3
-'''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
-'''
-
-import os
+"""distributes an archive to your web servers, using the function do_deploy"""
+from fabric.api import *
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from os.path import exists
+from os import mkdir
 
 
 env.hosts = ['52.201.219.192', '100.25.143.187']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    today = datetime.now()
+    year = today.year
+    month = today.month
+    day = today.day
+    hours = today.hour
+    minutes = today.minute
+    seconds = today.second
+    target = "versions/web_static_{}{}{}{}{}{}.tgz".\
+             format(year, month, day, hours, minutes, seconds)
+    if not exists('versions'):
+        mkdir('versions')
+    local("tar -zcvf {} web_static".format(target))
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
-
-def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
+    """distributes an archive to your web servers, using the
+    function do_deploy"""
+    if not exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+    temp = archive_path.split("/")[-1]
+    filename = archive_path
+    if temp:
+        filename = temp
+    tmp_archive = '/tmp/{}'.format(filename)
+    result = put(archive_path, tmp_archive)
+    if result.failed:
+        return False
+    targetFile = filename.split(".")[0]
+    deploy_path = '/data/web_static/releases/{}'.format(targetFile)
+    result = run("mkdir -p {}".format(deploy_path))
+    if result.failed:
+        return False
+    result = run("tar zxvf {}".format(tmp_archive))
+
+    result = run("mv ~/web_static/* {}".format(deploy_path))
+    if result.failed:
+        return False
+
+    if result.failed:
+        return False
+    if result.failed:
+        return False
+    result = sudo("rm -f {}".format(tmp_archive))
+    if result.failed:
+        return False
+    result = sudo("rm -f /data/web_static/current")
+    if result.failed:
+        return False
+    result = sudo("ln -s {} /data/web_static/current".format(deploy_path))
+    return True
